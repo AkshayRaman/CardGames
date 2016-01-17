fcb7583cdfbe9611984928b593716dc686b0cdca
Akshay <akshay.r007007@gmail.com>
2016-01-17 23:22:49 +0530

    Players now play the best card, not the first one

--- sevens.py
+++ sevens.py
@@ -3,6 +3,7 @@
 
 from random import shuffle
 import sys
+import math
 
 suits = ["hearts", "spades", "diamonds", "clubs"]
 num_cards = 13
@@ -79,12 +80,47 @@ class Game:
 				op += "%s - %s; " %(Card(suit, min), Card(suit, max))
 		print "Played cards: %s" %op
 	
-	def play_card(self, player, card):
+	def card_can_be_played(self, card):
+		if card.number == 7:
+			return True
+		if self.played_cards[card.suit]:
+			return (self.played_cards[card.suit][0] == card.number+1) or (self.played_cards[card.suit][1] == card.number-1)
+		return False
+		
+	def get_most_loss(self, player, card):
+		max_loss = 0
+		for c in self.player_hands[player]:
+				if c != card and c.suit == card.suit:
+					max_loss += int(c.number * math.fabs(card.number - c.number))
+		return max_loss
+	
+	def best_card_to_play(self, player):
+		card_loss_map = {}
+		for card in self.player_hands[player]:
+			if self.card_can_be_played(card):
+				most_loss = self.get_most_loss(player, card)
+				card_loss_map[most_loss] = card
+		
+		if card_loss_map:
+			print "Player %s's choice: %s" %(player, [(j.__str__(),i) for i,j in card_loss_map.iteritems()])
+			return card_loss_map[max(card_loss_map.keys())]
+		return None
+	
+	def play_card(self, player, card=None):
+		if card:
+			return card
+		card_to_play = self.best_card_to_play(player)
+		if card_to_play:
+			return self.play_card_inner(player, card_to_play)
+		return None
+	
+	def play_card_inner(self, player, card):
 		suit = card.suit
 		num = card.number
+		max_loss = 0
 		if not self.played_cards[suit] and num==7:
-			print "Player %s played %s." %(player, card)
 			self.played_cards[suit].extend([7,7])
+			print "Player %s played %s." %(player, card)
 			if card in self.player_hands[player]:
 				self.player_hands[player].remove(card)
 			return card
@@ -107,33 +143,31 @@ class Game:
 			else:
 				return None
 		
+	
 
 if __name__ == "__main__":
 	g = Game(4)
 	print g
 	cur_player = g.get_starter()
-	g.play_card(cur_player,Card("hearts", 7))
-	#cur_player += 1
+	g.play_card(cur_player, Card("hearts", 7))
 	cur_player = (cur_player+1)%(g.player_count)
 
 	while True:
 		print g
 		for card in g.player_hands[cur_player]:
-			if g.play_card(cur_player, card):
+			if g.play_card(cur_player):
 				#g.display_played_cards()
 				if len(g.player_hands[cur_player]) == 0:
 					print g
 					print "Player %s wins!!" %(cur_player)
-				
+					
 					for player, hand in enumerate(g.player_hands):
 						score = sum([i.number for i in hand])
 						print "Player: %s -> Score: %s" %(player, score)
 					sys.exit(0)
-				
-				#cur_player += 1
+					
 				cur_player = (cur_player+1)%(g.player_count)
 				break
 				
 		print "%s said pass." %(cur_player)
-		#cur_player += 1
 		cur_player = (cur_player+1)%(g.player_count)
